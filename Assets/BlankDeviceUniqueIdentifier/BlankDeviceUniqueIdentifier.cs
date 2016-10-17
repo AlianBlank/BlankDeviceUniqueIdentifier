9c43755a1922dde12ef9d3a4916abf822c67adfd
blank <wangfj11@foxmail.com>
2016-10-17 13:55:54 +0800

    Commit Init    提交 获取唯一机器码的插件

---
+++ Assets/BlankDeviceUniqueIdentifier/BlankDeviceUniqueIdentifier.cs
@@ -0,0 +1,74 @@
+﻿// /**
+//  *　　　　　　　　┏┓　　　┏┓+ +
+//  *　　　　　　　┏┛┻━━━┛┻┓ + +
+//  *　　　　　　　┃　　　　　　　┃ 　
+//  *　　　　　　　┃　　　━　　　┃ ++ + + +
+//  *　　　　　　 ████━████ ┃+
+//  *　　　　　　　┃　　　　　　　┃ +
+//  *　　　　　　　┃　　　┻　　　┃
+//  *　　　　　　　┃　　　　　　　┃ + +
+//  *　　　　　　　┗━┓　　　┏━┛
+//  *　　　　　　　　　┃　　　┃　　　　　　　　　　　
+//  *　　　　　　　　　┃　　　┃ + + + +
+//  *　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+//  *　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+//  *　　　　　　　　　┃　　　┃
+//  *　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+//  *　　　　　　　　　┃　 　　┗━━━┓ + +
+//  *　　　　　　　　　┃ 　　　　　　　┣┓
+//  *　　　　　　　　　┃ 　　　　　　　┏┛
+//  *　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+//  *　　　　　　　　　　┃┫┫　┃┫┫
+//  *　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+//  *
+//  *
+//  * ━━━━━━感觉萌萌哒━━━━━━
+//  * 
+//  * 说明：
+//  * 
+//  * 文件名：BlankDeviceUniqueIdentifier.cs
+//  * 创建时间：2016年08月16日 
+//  * 创建人：Blank Alian
+//  */
+using UnityEngine;
+#if UNITY_IOS || UNITY_IPHONE
+using System.Runtime.InteropServices;
+#endif
+/// <summary>
+/// 获取Android 和 IOS 的唯一机器码
+/// </summary>
+public class BlankDeviceUniqueIdentifier
+{
+
+    #region 获取设备机器码
+
+#if UNITY_IOS || UNITY_IPHONE
+
+        [DllImport("__Internal")]
+        private static extern string DeviceUniqueId();
+#endif
+
+    /// <summary>
+    /// 获取设备机器码
+    /// </summary>
+    /// <returns></returns>
+    public static string DeviceUniqueIdentifier
+    {
+        get
+        {
+            string id = PlayerPrefs.GetString("DeviceUniqueIdentifierID");
+            if (string.IsNullOrEmpty(id) || id == "null" || id.Length < 4)
+            {
+#if UNITY_IOS || UNITY_IPHONE
+		            string sid = DeviceUniqueId();
+#else
+                string sid = SystemInfo.deviceUniqueIdentifier;
+#endif
+                PlayerPrefs.SetString("DeviceUniqueIdentifierID", sid);
+                return sid;
+            }
+            return id;
+        }
+    }
+    #endregion
+}
---
+++ Assets/BlankDeviceUniqueIdentifier/BlankDeviceUniqueIdentifierExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlankDeviceUniqueIdentifierExample : MonoBehaviour {
+
+	private string id;
+	void OnGUI () {
+		if (GUILayout.Button (" GET DeviceUniqueIdentifier ", GUILayout.Width (200), GUILayout.Height (200))) {
+			id = BlankDeviceUniqueIdentifier.DeviceUniqueIdentifier;
+		}
+		GUILayout.Label (" DeviceUniqueIdentifier : "+ id);
+	}
+}
