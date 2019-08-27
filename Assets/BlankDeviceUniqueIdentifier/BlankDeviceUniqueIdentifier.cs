efa9b1425f1b7206ecdad187a23e91ff7721a3fa
Blank <blank@702.com>
2019-08-27 17:18:36 +0800

    【增加】【修改】 1. 修改Android 的设备码新方案 2. 增加32位机器码的限制,固定为32位

--- Assets/BlankDeviceUniqueIdentifier/BlankDeviceUniqueIdentifier.cs
+++ Assets/BlankDeviceUniqueIdentifier/BlankDeviceUniqueIdentifier.cs
@@ -24,7 +24,7 @@
 //  *
 //  * ━━━━━━感觉萌萌哒━━━━━━
 //  * 
-//  * 说明：
+//  * 说明：  
 //  * 
 //  * 文件名：BlankDeviceUniqueIdentifier.cs
 //  * 创建时间：2016年08月16日 
@@ -39,6 +39,14 @@ using System.Runtime.InteropServices;
 /// </summary>
 public class BlankDeviceUniqueIdentifier
 {
+
+    /**
+     *   Android 资料来源 https://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id/5626208#5626208
+     *
+     *   iOS  资料来源于 支付宝使用的 SSKeyChain  钥匙串存储
+     *   
+     */
+
 
     #region 获取设备机器码
 
@@ -59,11 +67,23 @@ public class BlankDeviceUniqueIdentifier
             string id = PlayerPrefs.GetString("DeviceUniqueIdentifierID");
             if (string.IsNullOrEmpty(id) || id == "null" || id.Length < 4)
             {
+#if UNITY_EDITOR || UNITY_STANDALONE 
+                string sid = SystemInfo.deviceUniqueIdentifier;
+#else
+
 #if UNITY_IOS || UNITY_IPHONE
-		            string sid = DeviceUniqueId();
+                string sid = DeviceUniqueId();
+#elif UNITY_ANDROID
+                AndroidJavaObject androidJavaObject = new AndroidJavaObject("com.alianhome.deviceuniqueidentifier.MainActivity");
+                string sid = androidJavaObject.CallStatic<string>("DeviceUniqueIdentifier");
 #else
                 string sid = SystemInfo.deviceUniqueIdentifier;
 #endif
+
+
+#endif
+                sid = sid.Replace("-", "");
+                sid = sid.Substring(0, 32);
                 PlayerPrefs.SetString("DeviceUniqueIdentifierID", sid);
                 return sid;
             }
